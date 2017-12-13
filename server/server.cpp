0e13ba02d3d44bea18f17dc642d4d3c01931509b
saloed <napster_1997@mail.ru>
2017-12-13 03:46:07 +0300

    udp server alpha

--- server/server.cpp
+++ server/server.cpp
@@ -7,19 +7,19 @@
 
 void server::Server::create_server_socket() {
     WSADATA wsa{};
-    auto init_status = WSAStartup(MAKEWORD(2,2),&wsa);
+    auto init_status = WSAStartup(MAKEWORD(2, 2), &wsa);
 
     if (init_status != 0) {
         Logger::logger_inst->error("WSA init failed with code {}", WSAGetLastError());
         std::exit(EXIT_FAILURE);
     }
-    auto server_d = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP );
-    if(server_d == INVALID_SOCKET){
+    auto server_d = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (server_d == INVALID_SOCKET) {
         Logger::logger_inst->error("Error in socket creation {}", WSAGetLastError());
         std::exit(EXIT_FAILURE);
     }
     BOOL fFlag = TRUE;
-    auto reuse_status = setsockopt(server_d,SOL_SOCKET, SO_REUSEADDR, (char *)&fFlag, sizeof(fFlag));
+    auto reuse_status = setsockopt(server_d, SOL_SOCKET, SO_REUSEADDR, (char *) &fFlag, sizeof(fFlag));
     if (reuse_status == SOCKET_ERROR) {
         Logger::logger_inst->error("Error in set socket reuse addr{}", WSAGetLastError());
         std::exit(EXIT_FAILURE);
@@ -27,93 +27,35 @@ void server::Server::create_server_socket() {
     sockaddr_in server{};
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( SERVER_PORT );
+    server.sin_port = htons(SERVER_PORT);
 
-    auto bind_status = bind(server_d , (sockaddr *)&server , sizeof(sockaddr_in));
-    if(bind_status == SOCKET_ERROR){
+    auto bind_status = bind(server_d, (sockaddr *) &server, sizeof(sockaddr_in));
+    if (bind_status == SOCKET_ERROR) {
         Logger::logger_inst->error("Error in socket bind {}", WSAGetLastError());
         std::exit(EXIT_FAILURE);
     }
-
-    server_socket = server_d;
-    read_event_d = CreateEvent(NULL, TRUE, TRUE, "ReadEvent");
-    completion_port = CreateIoCompletionPort (INVALID_HANDLE_VALUE, NULL, 0, 1);
-    if(!completion_port){
-        Logger::logger_inst->error("Error in completion port creation");
+    u_long argp = 1;
+    auto nonblock_status = ioctlsocket(server_d, 0x8004667E, &argp);
+    if (nonblock_status == SOCKET_ERROR) {
+        Logger::logger_inst->error("Error in setting socket nonblock {}", WSAGetLastError());
         std::exit(EXIT_FAILURE);
     }
-    CreateIoCompletionPort((HANDLE)server_d, completion_port, (DWORD)server_d, 2);
+    server_socket = server_d;
     Logger::logger_inst->info("Server initialized");
 }
 
-void server::Server::close_client(int client_d) {
-    std::unique_lock<std::mutex> lock(clients_mutex);
+void server::Server::close_client(int64_t client_d) {
+    lock_clients();
     if (clients.find(client_d) == clients.end())
         return;
     auto &&client = clients[client_d];
     clients.erase(client_d);
-    closesocket(client.descriptor);
-    client.is_active = false;
-    lock.unlock();
+    unlock_clients();
     Logger::logger_inst->info("Client {} disconnected", client_d);
 }
 
-void server::Server::accept_client() {
-//    sockaddr_in client_addr{};
-//    auto &&client_addr_in = reinterpret_cast<sockaddr *>(&client_addr);
-//    auto &&client_addr_len = static_cast<socklen_t>(sizeof(sockaddr));
-//    auto &&client_d = accept(server_socket, client_addr_in, &client_addr_len);
-//    if (client_d == -1) {
-//        if (errno != EAGAIN && errno != EWOULDBLOCK) {
-//            Logger::logger_inst->error("Accept failed");
-//        }
-//        return;
-//    }
-//    if (!socket_utils::set_socket_nonblock(client_d)) {
-//        Logger::logger_inst->error("Cannot set client socket {} nonblock", client_d);
-//        return;
-//    }
-//
-//    std::string client_info = inet_ntoa(client_addr.sin_addr);
-//    std::unique_lock<std::mutex> lock(clients_mutex);
-//    clients[client_d] = Client(client_d, event, client_info);
-//    lock.unlock();
-//    Logger::logger_inst->info("New connection from {} on socket {}", client_info, client_d);
-}
 
-void server::Server::read_client_data(int client_id) {
-    char read_buffer[MESSAGE_SIZE];
-    auto &&count = 0; //read(client_id, read_buffer, MESSAGE_SIZE);
-    if (count == -1 && errno == EAGAIN) return;
-    if (count == -1) Logger::logger_inst->error("Error in read for socket {}", client_id);
-    if (count <= 0) {
-        close_client(client_id);
-        return;
-    }
-    auto &&client = clients[client_id];
-    client.receive_buffer.append(read_buffer, static_cast<unsigned long>(count));
-}
-
-void server::Server::handle_client_if_possible(int client_id) {
-    auto &&client = clients[client_id];
-    auto &&message_end = client.receive_buffer.find(MESSAGE_END);
-    if (message_end != std::string::npos) {
-        auto &&message = client.receive_buffer.substr(0, message_end);
-        client.receive_buffer.erase(0, message_end + strlen(MESSAGE_END));
-        workers.enqueue(&Server::process_client_message, this, message, client_id);
-    }
-}
-
-
-void send_message(int client_id, std::string_view message) {
-    auto &&send_stat = send(client_id, message.data(), message.length(), 0);
-    if (send_stat == -1) {
-        Logger::logger_inst->error("Error in send for id {}", client_id);
-    }
-}
-
-
-void server::Server::process_client_message(std::string &message, int client_id) {
+void server::Server::process_client_message(std::string &message, int64_t client_id) {
     Logger::logger_inst->info(message);
     std::string_view message_view(message);
     if (message_view.compare(0, MESSAGE_PREFIX_LEN, CMD_PREFIX) == 0) {
@@ -133,14 +75,14 @@ void server::Server::process_client_message(std::string &message, int client_id)
 }
 
 
-void server::Server::process_client_text(std::string_view text, int client_id) {
+void server::Server::process_client_text(std::string_view text, int64_t client_id) {
     Logger::logger_inst->info("Text from client {}: {}", client_id, text.data());
     auto &&to_send = std::string(text) + MESSAGE_END;
     send_message(client_id, to_send);
 }
 
 
-void server::Server::process_add_currency(std::string &currency, int client_id) {
+void server::Server::process_add_currency(std::string &currency, int64_t client_id) {
     Logger::logger_inst->info("Client {} add currency {}", client_id, currency);
     auto &&status = database.add_currency(currency);
     if (status == 0) {
@@ -155,7 +97,7 @@ void server::Server::process_add_currency(std::string &currency, int client_id) 
     }
 }
 
-void server::Server::process_add_currency_value(std::string &currency, double value, int client_id) {
+void server::Server::process_add_currency_value(std::string &currency, double value, int64_t client_id) {
     Logger::logger_inst->info("Client {} add currency {} value {}", client_id, currency, value);
     auto &&status = database.add_currency_value(currency, value);
     if (status == 0) {
@@ -170,7 +112,7 @@ void server::Server::process_add_currency_value(std::string &currency, double va
     }
 }
 
-void server::Server::process_del_currency(std::string &currency, int client_id) {
+void server::Server::process_del_currency(std::string &currency, int64_t client_id) {
     Logger::logger_inst->info("Client {} del currency {}", client_id, currency);
     auto &&status = database.del_currency(currency);
     if (status == 0) {
@@ -185,7 +127,7 @@ void server::Server::process_del_currency(std::string &currency, int client_id) 
     }
 }
 
-void server::Server::process_list_all_currencies(int client_id) {
+void server::Server::process_list_all_currencies(int64_t client_id) {
     Logger::logger_inst->info("Client {} list all currencies", client_id);
     nlohmann::json json_response;
     auto &&status = database.currency_list(json_response);
@@ -198,7 +140,7 @@ void server::Server::process_list_all_currencies(int client_id) {
     }
 }
 
-void server::Server::process_currency_history(std::string &currency, int client_id) {
+void server::Server::process_currency_history(std::string &currency, int64_t client_id) {
     nlohmann::json json_response;
     auto &&status = database.currency_history(currency, json_response);
     if (status == 0) {
@@ -213,7 +155,7 @@ void server::Server::process_currency_history(std::string &currency, int client_
     }
 }
 
-void server::Server::process_client_command(std::string_view command, int client_id) {
+void server::Server::process_client_command(std::string_view command, int64_t client_id) {
     Logger::logger_inst->info("Command from client {}: {}", client_id, command.data());
     if (command == "disconnect") {
         close_client(client_id);
@@ -227,7 +169,7 @@ void server::Server::process_client_command(std::string_view command, int client
 }
 
 
-void server::Server::process_client_json(std::string_view json_string, int client_id) {
+void server::Server::process_client_json(std::string_view json_string, int64_t client_id) {
     Logger::logger_inst->info("Json from client {}: {}", client_id, json_string.data());
     try {
         auto &&client_json = nlohmann::json::parse(json_string);
@@ -257,88 +199,180 @@ void server::Server::process_client_json(std::string_view json_string, int clien
 
 }
 
-void server::Server::client_message_chunk(sockaddr_storage client_info, int chunk_number, int total, std::string& message){
-
+void server::Server::handle_client_if_possible(int64_t client_id) {
+    std::vector<std::string>* recv_buffer = &clients[client_id].receive_buffer;
+    std::string received_message;
+    for(auto&& chunk : *recv_buffer){
+        received_message += chunk;
+    }
+    auto &&message_end = received_message.find(MESSAGE_END);
+    if (message_end != std::string::npos) {
+        auto &&message = received_message.substr(0, message_end);
+        workers.enqueue(&Server::process_client_message, this, message, client_id);
+    } else{
+        Logger::logger_inst->error("Incorrect message received from client {}", client_id);
+    }
+    recv_buffer->clear();
 }
 
-void server::Server::refresh_client_timeout(sockaddr_storage client_info){
-
+void server::Server::send_message(int64_t client_id, std::string_view message) {
+    std::vector<std::string>* send_buffer = &clients[client_id].send_buffer;
+    send_buffer->clear();
+    auto size = message.size() / UDP_PACKET_SIZE + 1;
+    for(auto i = 0, j = 0; i < size; i += UDP_PACKET_SIZE, ++j) {
+        auto chunk = message.substr(i, UDP_PACKET_SIZE);
+        char info_str[sizeof(int) * 2 + 1];
+        info_str[0] = CONTENT_MESSAGE;
+        auto packet_info = (int *)(info_str + 1);
+        packet_info[0] = j;
+        packet_info[1] = size;
+        auto packet = std::string(info_str) + std::string(chunk);
+        send_buffer->emplace_back(packet);
+    }
+    auto client_addr = clients[client_id].ip_addr;
+    for(auto&& packet: *send_buffer) {
+        send_chunk(client_addr, packet);
+    }
 }
 
-void server::Server::client_message_status(sockaddr_storage client_info, int chunk_number, int status){
+void server::Server::send_chunk(int64_t client_id, std::string_view message){
+    auto client_addr = clients[client_id].ip_addr;
+    send_chunk(client_addr, message);
+}
 
+void server::Server::send_chunk(sockaddr_in& client_addr, std::string_view message){
+    auto addr = reinterpret_cast<sockaddr*>(&client_addr);
+    auto &&send_stat = sendto(server_socket, message.data(), message.size(), 0, addr, sizeof(sockaddr));
+    if (send_stat == SOCKET_ERROR) {
+        Logger::logger_inst->error("Error in send {}", WSAGetLastError());
+    }
+}
+
+void server::Server::client_message_chunk(int64_t client_id, int chunk_number, int total, std::string &message) {
+    std::vector<std::string>* recv_buffer = &clients[client_id].receive_buffer;
+    if(chunk_number == 0){
+        recv_buffer->clear();
+    }
+    auto expected_message = recv_buffer->size();
+    if(chunk_number < expected_message){
+        return;
+    }
+    if(chunk_number > expected_message){
+        char info_str[sizeof(int) + 1];
+        info_str[0] = CHUNK_REQUEST_MESSAGE;
+        auto packet_info = (int *)(info_str + 1);
+        packet_info[0] = expected_message;
+        auto packet = std::string(info_str);
+        send_chunk(client_id, packet);
+        return;
+    }
+    recv_buffer->emplace_back(message);
+    if(chunk_number == total){
+        char info_str[sizeof(int) + 1];
+        info_str[0] = CHUNK_SUCCESS_MESSAGE;
+        auto packet_info = (int *)(info_str + 1);
+        packet_info[0] = total;
+        auto packet = std::string(info_str);
+        send_chunk(client_id, packet);
+    }
+    if(recv_buffer->size() == total){
+        handle_client_if_possible(client_id);
+    }
+}
+
+void server::Server::refresh_client_timeout(int64_t client_id) {
+    clients[client_id].timer = 0;
+}
+
+void server::Server::client_message_status(int64_t client_id, int chunk_number, int status) {
+    std::vector<std::string>* send_buffer = &clients[client_id].send_buffer;
+    if (send_buffer->empty()) return;
+    if(status == CHUNK_SUCCESS_MESSAGE && chunk_number == send_buffer->size()){
+        return send_buffer->clear();
+    }
+    if (status == CHUNK_REQUEST_MESSAGE && chunk_number < send_buffer->size()){
+        return send_chunk(client_id, send_buffer->at(chunk_number));
+    }
 }
 
 
-int64_t get_id_for_client_info(sockaddr_in client_info){
-    int64_t result = client_info.sin_addr.s_addr;
-    return result << 32 | client_info.sin_port;
+int64_t get_id_for_client_info(sockaddr_in *client_addr) {
+    int64_t result = client_addr->sin_addr.s_addr;
+    return result << 32 | client_addr->sin_port;
 }
 
+int64_t server::Server::get_client_id(sockaddr_in *client_addr) {
+    auto id = get_id_for_client_info(client_addr);
+    if (clients.find(id) == std::end(clients)) {
+        char ip_buf[INET_ADDRSTRLEN];
+        inet_ntop(client_addr->sin_family, &client_addr->sin_addr, ip_buf, sizeof(ip_buf));
+        std::string ip_str(ip_buf);
+        lock_clients();
+        clients[id] = Client{id, ip_str, *client_addr};
+        unlock_clients();
+        Logger::logger_inst->info("New connection from {} with id {}", ip_str, id);
+    }
+    return id;
+}
 
-void server::Server::handle_client_datagram(WSAEVENT event){
+void server::Server::handle_client_datagram(WSAEVENT event) {
     WSANETWORKEVENTS network_events{};
-    sockaddr_storage client_info{};
-    socklen_t client_info_size = sizeof(sockaddr_storage);
+    sockaddr client_info{};
+    socklen_t client_info_size = sizeof(sockaddr);
     char receive_buffer[MESSAGE_SIZE + 1];
     WSAEnumNetworkEvents(server_socket, event, &network_events);
-    auto bytes = recvfrom(server_socket, receive_buffer, MESSAGE_SIZE, 0,
-                     (sockaddr*) &client_info, &client_info_size);
+    auto bytes = recvfrom(server_socket, receive_buffer, MESSAGE_SIZE, 0, &client_info, &client_info_size);
+    auto client_addr = reinterpret_cast<sockaddr_in*>(& client_info);
 
-    if(bytes < 0){
+    if (bytes < 0) {
         auto err_code = GetLastError();
-        if(err_code == WSAECONNRESET) return;
+        if (err_code == WSAECONNRESET) return;
         Logger::logger_inst->error("Error in recv {}", err_code);
         terminate = true;
     }
+    auto client_id = get_client_id(client_addr);
+    refresh_client_timeout(client_id);
 
-    if(bytes == 0){
-        return refresh_client_timeout(client_info);
-    }
+    if (bytes == 0)
+        return;
+
     char message_type = receive_buffer[0];
-    if(message_type == CHUNK_REQUEST_MESSAGE || message_type == CHUNK_SUCCESS_MESSAGE){
-        int message_number = *(int *)(receive_buffer + 1);
-        return client_message_status(client_info, message_number, message_type);
+    if (message_type == CHUNK_REQUEST_MESSAGE || message_type == CHUNK_SUCCESS_MESSAGE) {
+        int message_number = *(int *) (receive_buffer + 1);
+        return client_message_status(client_id, message_number, message_type);
     } else if (message_type == CONTENT_MESSAGE) {
-        auto * as_int_array = (int *)(receive_buffer + 1);
+        auto *as_int_array = (int *) (receive_buffer + 1);
         int message_number = as_int_array[0];
-        int message_total  = as_int_array[1];
+        int message_total = as_int_array[1];
         receive_buffer[bytes] = '\0';
-        char* content = receive_buffer + 1 + 2 * sizeof(int);
+        char *content = receive_buffer + 1 + 2 * sizeof(int);
         std::string message(content);
-        return client_message_chunk(client_info, message_number, message_total, message);
+        return client_message_chunk(client_id, message_number, message_total, message);
     } else {
         Logger::logger_inst->error("Unknown message type");
     }
-}
-
-void server::Server::disconnect_time_outed(){
-
 }
 
 void server::Server::serve_loop() {
     Logger::logger_inst->info("Server started on port {}", SERVER_PORT);
     WSAEVENT events[1] = {WSACreateEvent()};
     WSAEventSelect(server_socket, events[0], FD_READ);
-    while(!terminate)
-    {
+    while (!terminate) {
         auto result = WSAWaitForMultipleEvents(1, events, FALSE, 2000, FALSE);
-        switch(result)
-        {
+        switch (result) {
             case WAIT_TIMEOUT:
-                disconnect_time_outed();
                 break;
             case WAIT_OBJECT_0:
                 handle_client_datagram(events[0]);
                 break;
-            case WAIT_FAILED:{
+            case WAIT_FAILED: {
                 Logger::logger_inst->error("Select failed {}", result);
                 terminate = true;
                 break;
             }
-            default:{
+            default: {
                 Logger::logger_inst->error("Unexpected select event {}", result);
-               terminate = true;
+                terminate = true;
             }
         }
     }
@@ -352,12 +386,32 @@ void server::Server::stop() {
     if (server_thread.joinable()) {
         server_thread.join();
     }
+    if (timer_thread.joinable()) {
+        timer_thread.join();
+    }
     WSACleanup();
+}
+
+void server::Server::timer_loop() {
+    auto timer = CreateWaitableTimer(NULL, TRUE, "Server timer");
+    LARGE_INTEGER timer_time{};
+    timer_time.QuadPart = -100000000LL;
+    while (!terminate) {
+        SetWaitableTimer(timer, &timer_time, 0, NULL, NULL, 0);
+        WaitForSingleObject(timer, INFINITE);
+        for (auto &&entry: clients) {
+            ++entry.second.timer;
+            if (entry.second.timer > TIMEOUT_DELTA) {
+                close_client(entry.first);
+            }
+        }
+    }
 }
 
 void server::Server::start() {
     terminate = false;
     server_thread = std::move(std::thread(&Server::serve_loop, this));
+    timer_thread = std::move(std::thread(&Server::timer_loop, this));
 }
 
 bool server::Server::is_active() {
@@ -375,10 +429,10 @@ void server::Server::close_all_clients() {
 std::string server::Server::list_clients() {
     std::stringstream out_string;
     out_string << "Clients connected:";
-    for (auto && entry: clients) {
-        auto&& client_id = entry.first;
-        auto&& client = entry.second;
-        out_string << "\nid: " << client_id << " " << client.client_ip_addr;
+    for (auto &&entry: clients) {
+        auto &&client_id = entry.first;
+        auto &&client = entry.second;
+        out_string << "\nid: " << client_id << " " << client.ip_str;
     }
     return out_string.str();
 }
--- server/server.h
+++ server/server.h
@@ -14,78 +14,73 @@
 #include "database/FinanceDb.h"
 #include "defines.h"
 
-typedef int epoll_event;
+#define TIMEOUT_DELTA 30
+#define UDP_PACKET_SIZE 256
 
 namespace server {
     class Client {
     public:
-        Client() : descriptor(0), is_active(false) {
-            mutex = std::make_unique<std::mutex>();
-        }
+        Client() : descriptor(0), timer(0), ip_addr{} {}
 
-        explicit Client(SOCKET descriptor, std::string &client_ip) :
-                descriptor(descriptor), is_active(true), client_ip_addr(client_ip) {
-            mutex = std::make_unique<std::mutex>();
-        }
+        explicit Client(int64_t descriptor, std::string &ip_str, sockaddr_in& ip_addr) :
+                descriptor(descriptor), timer(0), ip_str(ip_str), ip_addr(ip_addr) {}
 
         Client &operator=(Client &&other) noexcept {
             if (this != &other) {
                 descriptor = other.descriptor;
-                is_active = other.is_active.load();
-                mutex = std::move(other.mutex);
                 receive_buffer = std::move(other.receive_buffer);
                 send_buffer = std::move(other.send_buffer);
+                timer = other.timer;
+                ip_str = std::move(other.ip_str);
+                ip_addr = other.ip_addr;
             }
             return *this;
         }
 
-        SOCKET descriptor;
-        volatile std::atomic_bool is_active;
+        int64_t descriptor;
+        int timer;
         std::vector<std::string> send_buffer;
         std::vector<std::string> receive_buffer;
-        std::string client_ip_addr;
-        std::unique_ptr<std::mutex> mutex;
+        std::string ip_str;
+        sockaddr_in ip_addr;
     };
 
 
     class Server {
 
     public:
-        Server() : server_socket(0), terminate(false), workers(4), database(),
-                   read_event_d{}, completion_port{} {
+        Server() : server_socket(0), terminate(false), workers(4), database(), clients_lock() {
             create_server_socket();
+            InitializeCriticalSection(&clients_lock);
         }
 
         ~Server() {
             stop();
+            DeleteCriticalSection(&clients_lock);
         }
 
     private:
         void create_server_socket();
 
-        void accept_client();
+        void handle_client_if_possible(int64_t client_id);
 
-        void read_client_data(int client_id);
+        void process_client_message(std::string &message, int64_t client_id);
 
-        void handle_client_if_possible(int client_id);
+        void process_client_command(std::string_view command, int64_t client_id);
 
-        void process_client_message(std::string &message, int client_id);
+        void process_client_text(std::string_view text, int64_t client_id);
 
-        void process_client_command(std::string_view command, int client_id);
+        void process_client_json(std::string_view json_string, int64_t client_id);
 
-        void process_client_text(std::string_view text, int client_id);
+        void process_add_currency(std::string &currency, int64_t client_id);
 
-        void process_client_json(std::string_view json_string, int client_id);
+        void process_add_currency_value(std::string &currency, double value, int64_t client_id);
 
-        void process_add_currency(std::string &currency, int client_id);
+        void process_del_currency(std::string &currency, int64_t client_id);
 
-        void process_add_currency_value(std::string &currency, double value, int client_id);
+        void process_list_all_currencies(int64_t client_id);
 
-        void process_del_currency(std::string &currency, int client_id);
-
-        void process_list_all_currencies(int client_id);
-
-        void process_currency_history(std::string &currency, int client_id);
+        void process_currency_history(std::string &currency, int64_t client_id);
 
         void serve_loop();
 
@@ -96,7 +91,7 @@ namespace server {
 
         bool is_active();
 
-        void close_client(int client_d);
+        void close_client(int64_t client_d);
 
         void close_all_clients();
 
@@ -105,7 +100,8 @@ namespace server {
     private:
         std::unordered_map<int64_t , Client> clients;
         std::thread server_thread;
-        std::mutex clients_mutex;
+        std::thread timer_thread;
+        CRITICAL_SECTION clients_lock;
         ThreadPool workers;
         FinanceDb database;
         volatile std::atomic_bool terminate;
@@ -113,13 +109,29 @@ namespace server {
 
         void handle_client_datagram(WSAEVENT);
 
-        void disconnect_time_outed();
+        void lock_clients(){
+            EnterCriticalSection(&clients_lock);
+        }
 
-        void refresh_client_timeout(sockaddr_storage client_info);
+        void unlock_clients(){
+            LeaveCriticalSection(&clients_lock);
+        }
 
-        void client_message_status(sockaddr_storage client_info, int chunk_number, int status);
+        void refresh_client_timeout(int64_t client_id);
 
-        void client_message_chunk(sockaddr_storage client_info, int chunk_number, int total, std::string& message);
+        void client_message_status(int64_t client_id, int chunk_number, int status);
+
+        void client_message_chunk(int64_t client_id, int chunk_number, int total, std::string& message);
+
+        int64_t get_client_id(sockaddr_in *client_addr);
+
+        void timer_loop();
+
+        void send_message(int64_t client_id, std::string_view message);
+
+        void send_chunk(sockaddr_in &client_addr, std::string_view message);
+
+        void send_chunk(int64_t client_id, std::string_view message);
     };
 };
 
